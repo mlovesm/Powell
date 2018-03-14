54ccc7c639db5924c2fac6e7ce7e1d977842a488
mlovesm <mlovesm@naver.com>
2018-03-14 10:57:47 +0900

    Initial commit

---
+++ app/src/main/java/com/green/powell/app/fragment/BaseFragment.java
@@ -0,0 +1,55 @@
+package com.green.powell.app.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+import com.green.powell.app.util.UtilClass;
+
+/**
+ * Created by GS on 2017-08-21.
+ */
+public class BaseFragment extends Fragment {
+    public final String TAG = getClass().getSimpleName();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        UtilClass.logD(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        BusProvider.getInstance().register(this);
+    }
+
+    @Override
+    public void onResume() {
+        UtilClass.logD(TAG, "onResume");
+        super.onResume();
+    }
+
+
+    @Override
+    public void onDestroy() {
+        UtilClass.logD(TAG, "onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDestroyView() {
+        BusProvider.getInstance().unregister(this);
+        super.onDestroyView();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        UtilClass.logD(TAG, "onStart");
+
+    }
+}
